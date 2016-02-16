1c26e782b95098c1381134f83a1d19dc051ec423
Silvio Bernardinello <silvio.bernardinello@gmail.com>
2016-02-16 22:02:41 +0100

    Refact

--- src/main/java/main/RemoteCommanderMain.java
+++ src/main/java/main/RemoteCommanderMain.java
@@ -13,24 +13,31 @@ import services.TicketManagerService;
 import spark.Spark;
 
 public class RemoteCommanderMain {
-	@Parameter(names="-ttl")
+	@Parameter(names = "-ttl", description = "Number of minutes before results expire")
 	int ttl = 60;
-	@Parameter(names="-port")
+	@Parameter(names = "-port", description = "Port where to bind the server")
 	int port = 2220;
-	@Parameter(names={"-threads", "-t"})
+	@Parameter(names = { "-threads", "-t" }, description = "Number of threads to uses")
 	int threads = Runtime.getRuntime().availableProcessors();
-	
+	@Parameter(names = "--help", help = true)
+	private boolean help = false;
+
 	public static void main(String[] args) {
 		RemoteCommanderMain main = new RemoteCommanderMain();
-		new JCommander(main, args);
+		JCommander jcm = new JCommander(main, args);
+
+		if( main.help ) {
+			jcm.usage();
+			return;
+		}
 		main.run();
 	}
-	
+
 	public void run() {
 		Spark.port(2220);
 		ExecutorService executorPool = Executors.newFixedThreadPool(threads);
 		TicketManagerService resultManager = new TicketManagerService(ttl, TimeUnit.MINUTES);
-		
+
 		new CommandResource(executorPool, resultManager);
 		new ResultsResource(resultManager);
 	}
